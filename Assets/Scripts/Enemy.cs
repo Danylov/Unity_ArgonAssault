9ada64589837c2ecde3bb5ca2bfe32f5d5069355
Danylov <ol_danylov@ukr.net>
2022-06-06 15:15:45 +0300

    Add Enemies to the scene.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,18 +7,26 @@ public class Enemy : MonoBehaviour
 {
    [SerializeField] GameObject deathVFX; 
    [SerializeField] GameObject hitVFX; 
-   [SerializeField] Transform parent;
 
    [SerializeField] int scorePerHit = 15;
    [SerializeField] int hitPoints = 4;
    
+   GameObject parentGameObject;
    ScoreBoard scoreBoard;
 
    void Start()
    {
       scoreBoard = FindObjectOfType<ScoreBoard>();
+      parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+      AddRigidbody();
    }
-   
+
+   void AddRigidbody()
+   {
+      var rb = gameObject.AddComponent<Rigidbody>();
+      rb.useGravity = false;
+   }
+
    void OnParticleCollision(GameObject other)
    {
       ProcessHit();
@@ -27,15 +35,15 @@ public class Enemy : MonoBehaviour
 
    void ProcessHit()
    {
-      GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-      vfx.transform.parent = parent;
+      var vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+      vfx.transform.parent = parentGameObject.transform;
       hitPoints--;
       scoreBoard.IncreaseScore(scorePerHit);
    }
    void KillEnemy()
    {
-      GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-      vfx.transform.parent = parent;
+      var vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+      vfx.transform.parent = parentGameObject.transform;
       Destroy(gameObject);
    }
 
